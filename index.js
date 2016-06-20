bdc0b9c28f8266e6af3036dbbc76ea99a4629a25
王。 <atmwjf@gmail.com>
2016-06-20 14:38:40 +0800

    md5字体名称

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fontCarrier = require('font-carrier');
 var filePlus = require('file-plus');
 var fs = require('fs');
 var path = require('path');
-
+var crypto = require('crypto');
 var fontType = {
 	'eot':'embedded-opentype',
 	'woff':'woff',
@@ -12,16 +12,27 @@ var fontType = {
 }
 class Svg {
 
-	constructor(svgDir,tname,startChar){
-		this.name = tname;
+	// constructor(svgDir,tname,startChar){
+	constructor(config){
+		// config = {
+		// 	svgDir:'', //svg目录
+		// 	name:'',//字体名称
+		// 	nameToMD5:true, //字体文件md5命名
+		// 	startChar:''	//字符起始
+		// }
+		if(!config.nameToMD5){
+			this.name = config.name;
+		}
+		this.fontFaceName = config.fontFaceName;
+		this.nameToMD5 = config.nameToMD5;
+		//this.name = tname;
 		this.infos = [];
 		this.font = fontCarrier.create();
 		var self = this;
 		var char = "&#xe";
-		startChar = startChar || '900';
-		var self = this;
+		var startChar = config.startChar || '900';
 
-		var files = filePlus.getAllFilesSync(svgDir);
+		var files = filePlus.getAllFilesSync(config.svgDir);
 		if(files.length == 0){
 			return null;
 		}
@@ -38,7 +49,6 @@ class Svg {
 			startChar = self.next(startChar);
 			self.infos.push(tempJson);
 		})
-
 	}
 
 	getInfos(){
@@ -48,18 +58,35 @@ class Svg {
 	 * 输出字体文件
 	 * @param  {[type]} outPut [description]
 	 * @param  {[type]} name   [description]
-	 * @return {[type]}        [description]
+	 * @return {String} 文件名   [description]
 	 */
 	outPut(outPut){
 		if(!fs.existsSync(outPut)){
 			//创建目录
 			filePlus.mkdirsSync(outPut,'0777');
 		}
+		//var fontPath = path.join(outPut,this.name);
+		// var fontBuffer = this.font.output({
+		//   path:fontPath
+		// });
+		if(this.nameToMD5){
+			var fontBuffer = this.font.output();
+			var hash = crypto.createHash('md5').update(fontBuffer.svg.toString("utf-8")).digest('hex');
+			this.name = hash.substring(0,6);
+			if(fs.existsSync(path.join(outPut,this.name+'.svg'))){
+				console.log('字体未改变');
+				return this.name
+			}
+		}
 		var fontPath = path.join(outPut,this.name);
-		this.font.output({
-		  path:fontPath
-		})
-		return true;
+
+		this.font.output(
+			{
+				path:fontPath
+			}
+		);
+		//console.log(aa);
+		return this.name;
 	}
 	/**
 	 * 获取下一个字符码
@@ -79,16 +106,16 @@ class Svg {
 	 * @return {String}        @font-face属性
 	 */
 	getClassHead(urlPath,version,only64){
-		var fontFace = '@font-face {\n'+'font-family: "'+this.name+'";\n';
+		var fontFace = '@font-face {\n'+'font-family: "'+this.fontFaceName+'";\n';
 		var last = '';
 		if(only64){
 			var object = this.font.output();
 			fontFace = fontFace + 'src:url(ddata:application/x-font-ttf;base64,'+object['ttf'].toString('base64')+'format(\''+fontType['ttf']+'\'));\n'
 		}else{
-			fontFace = fontFace + 'src:url("'+urlPath+this.name+'.eot?v='+version+'");\nsrc:';
+			fontFace = fontFace + 'src:url("'+urlPath+this.name+'.eot");\nsrc:';
 			for(var item in fontType){
 				fontFace = fontFace + last;
-				fontFace = fontFace + 'url("'+urlPath+this.name+'.'+item+'?v='+version+'") format("'+fontType[item]+'")';
+				fontFace = fontFace + 'url("'+urlPath+this.name+'.'+item+'") format("'+fontType[item]+'")';
 				last = ',\n';
 			}
 			fontFace = fontFace + ';\n';
